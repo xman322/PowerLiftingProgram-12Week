a768c9e7649818ce588c4d574f75789ba9e3551f
xman322 <xagarza@gmail.com>
2022-03-13 00:24:46 -0600

    added code

--- website/views.py
+++ website/views.py
@@ -4,25 +4,16 @@ from . import db
 from .models import Note, Maxes
 views = Blueprint('views', __name__)
 
-headings = ("name", "role", "salary")
-data=(("rolf", "product ownwer", "55k"), ("amy", "eng", "99k"), ("bob", "sec eng", "100k"), ("rolf", "product ownwer", "55k"), ("amy", "eng", "99k"), ("bob", "sec eng", "100k"))
+headings = ("Day 1", "Sets", "Reps", "Weight", "RPE")
+data=(("rolf", "product ownwer", "55k","5"), ("amy", "eng", "99k","5"), ("bob", "sec eng", "100k","5"), ("rolf", "product ownwer", "55k","5"))
+totals = []
+
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
    
-    if request.method == 'POST':
-        note = request.form.get('note')
-
-        if len(note) < 1:
-            flash('Note is too short', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added', category='success')
-           
-    return render_template("home.html" , user=current_user, headings = headings, data = data)
+    return render_template("home.html" , user=current_user, headings = headings, data = data, entries=totals)
 
 
 
@@ -31,6 +22,22 @@ def home():
 def starthere():
     
     if request.method == 'POST':
+        totals.append(float(request.form.get('benchmax')))
+        totals.append(float(request.form.get('squatmax')))
+        totals.append(float(request.form.get('deadmax')))
+        totals.append(float(request.form.get('psquatmax')))
+        totals.append(float(request.form.get('pdeadmax')))
+        """
+        totals.append(
+            (
+            float(request.form.get('benchmax')),
+            float(request.form.get('squatmax')),
+            float(request.form.get('deadmax')),
+            float(request.form.get('psquatmax')),
+            float(request.form.get('pdeadmax'))
+            )
+        )
+        
         benchmax = request.form.get('benchmax')
         squatmax = request.form.get('squatmax')
         deadmax = request.form.get('deadmax')
@@ -40,6 +47,7 @@ def starthere():
         new_max = Maxes(benchmax = benchmax, squatmax = squatmax, deadmax = deadmax, pdeadmax= pdeadmax, psquatmax=psquatmax, user_id = current_user.id)
         db.session.add(new_max)
         db.session.commit()
+        """
         return redirect(url_for('views.home'))
 
     return render_template("starthere.html", user=current_user)
